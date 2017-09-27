bed2fdb8c32fc6993d0d8c105fb4019d06998732
nitinsampath <nitinsampath@Nitins-MacBook-Pro.local>
2017-09-27 00:25:58 -0700

    finished hw2

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,5 +1,12 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
   def initialize(baz)
+  	@baz = baz
   end
+
+  def bar(s, hash)
+  	first = s.to_s
+  	first+@baz+hash[hash.keys[0]].to_s
+  end
+
 end
--- app/controllers/concerns/person.rb
+++ app/controllers/concerns/person.rb
@@ -8,17 +8,17 @@ class Person
 
   def nickname
     # YOUR IMPLEMENTATION HERE
-    'delete_me'
+    @nickname = name[0..3]
   end
 
   def birth_year
     # YOUR IMPLEMENTATION HERE
-    1990
+    2017 - @age.to_i
   end
 
   def introduction
     # YOUR IMPLEMENTATION HERE
-    'delete_me'
+    "name: " + @name + " age: " + @age
   end
 
 end
